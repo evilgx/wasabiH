9c38026b1c83a46b5eda5148fedf9d869018711b
apple <apple@apple.local>
2016-08-16 10:44:58 +0800

    modify

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 /**
  * Created by wangzhiyong on 16/8/16.
  */
+<<<<<<< HEAD
+
+exports.Button = require('./Buttons/Button.jsx');//
+exports.LinkButton = require('./Buttons/LinkButton.jsx');//
+exports.Cell = require('./Data/Cell.jsx');//
+exports.List = require('./Data/List.jsx');//
+exports.Article = require('./Layout/Article.jsx');//
+exports.Content = require('./Layout/Content.jsx');//
+exports.Footer = require('./Layout/Footer.jsx');//
+exports.Header = require('./Layout/Header.jsx');//
+exports.Page = require('./Layout/Page.jsx');//
+exports.Progress = require('./Layout/Progress.jsx');//
+exports.ActionSheet = require('./Navigation/ActionSheet.jsx');//
+exports.Navbar = require('./Navigation/Navbar.jsx');//
+exports.Tabs = require('./Navigation/Tabs.jsx');//
+exports.TabModel = require('./Model/TabModel.js');//
+=======
+>>>>>>> 511270ba12f7c749ea48f7766855dfe3240e70f9
