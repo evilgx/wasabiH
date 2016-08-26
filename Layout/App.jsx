d8cfa995801c5a6ba382753138c7ec59d00859b9
apple <apple@apple.local>
2016-08-26 15:44:58 +0800

    m

--- Layout/App.jsx
+++ Layout/App.jsx
@@ -39,7 +39,7 @@ class App {
                 <Route path="/" component={this.Root}>
                     {
                         this.routers.map((item, index)=> {
-                            return <Route path={item.url} component={item.app}></Route>
+                            return <Route key={"route"+index.toString()} path={item.url} component={item.app}></Route>
                         })
                     }
                 </Route>
@@ -51,4 +51,3 @@ class App {
 }
 
 module .exports=App;
-
