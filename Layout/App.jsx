a64b39aa648846456ab81e9a57fb9f39cbf197cb
apple <apple@apple.local>
2016-08-27 11:57:28 +0800

    m

--- Layout/App.jsx
+++ Layout/App.jsx
@@ -10,30 +10,50 @@ let Route=require("react-router").Route;
 let Link=require("react-router").Link;
 //
 class App {
-    constructor(routers=null) {
+    constructor(routers=null,home=null,menu=null,welcome=null) {
         if (routers) {
             this.routers = routers;
         }
         else {
             this.routers = [];
         }
+        this.menu=menu;
+        this.home=home;
+        this.welcome=welcome;
         this.Root = React.createClass({
             render(){
-                if(this.props.children)
+                if(this.welcome!=null)
                 {
-                    return <div className="root">
-                        <div className="aside_container"></div>
-                        <div className="section_container">
-                            {this.props.children}
+                    if(this.home!=null)
+                    {
+                        return <div className="root">
+                            <div className="aside_container">
+                                {this.menu}
+                            </div>
+                            <div className="section_container">
+                                {this.home}
+                            </div>
                         </div>
-                    </div>
+                    }
+                    else
+                    {
+                        return <div className="root">
+                            <div className="aside_container">
+                                {this.menu}
+                            </div>
+                            <div className="section_container">
+                                {this.home}
+                            </div>
+                        </div>
+                    }
+
                 }
                 else
                 {
                     return <div className="root">
-                        <div className="aside_container"></div>
+                        <div className="aside_container">{this.menu}</div>
                         <div className="section_container">
-                           <div><Link to="/Home">心怡科技欢迎您</Link></div>
+                           <div><Link to="/home">心怡科技欢迎您,进入主页</Link></div>
                         </div>
                     </div>
 
@@ -49,13 +69,24 @@ class App {
     }
 
     render() {
+
+        var routeArr=[];
+        if(this.home!=null)
+        {
+            routeArr.push(<Route key={"routeHome"} path={"/home"} component={this.home}></Route>);
+        }
+        if(this.welcome!=null)
+        {
+            routeArr.push(<Route key={"routeHome"} path={"/welcome"} component={this.welcome}></Route>);
+        }
+        this.routers.map((item, index)=> {
+            routeArr.push( <Route key={"route"+index.toString()} path={item.url} component={item.component}></Route>);
+        })
         ReactDOM.render(
             (<Router history={hashHistory}>
                 <Route path="/" component={this.Root}>
                     {
-                        this.routers.map((item, index)=> {
-                            return <Route key={"route"+index.toString()} path={item.url} component={item.component}></Route>
-                        })
+                 routeArr
                     }
                 </Route>
             </Router>),
