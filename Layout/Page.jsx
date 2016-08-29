b915f7ab40328dc2d08c17fd64b79652050f2958
apple <apple@apple.local>
2016-08-29 16:03:41 +0800

    m

--- Layout/Page.jsx
+++ Layout/Page.jsx
@@ -8,7 +8,7 @@ let IndexRoute=require("react-router").IndexRoute;
 let Page=React.createClass({
     propsType:{
         active:React.PropTypes.bool,//是否激活
-        routers:React.PropTypes.array,//子路由
+
     },
     getDefaultProps:function() {
         return {
@@ -30,27 +30,12 @@ let Page=React.createClass({
     },
     render :function() {
 
-        if (this.routers instanceof Array) {
-            var routeArr=[<IndexRoute key="subhome" component={this.state.routers[0].compoment}/>];
-
-            this.routers.map((item, index)=> {
-                routeArr.push( <Route key={"route"+index.toString()} path={item.url} component={item.component}></Route>);
-            })
-            return (<section className={"wasabi-page "+this.state.active}>
-                {this.props.header}
-                {
-                  routeArr
-                }
-                {this.props.footer}
-            </section>)
-        }
-        else {
             return (<section className={"wasabi-page "+this.state.active}>
                 {this.props.header}
                 {this.props.children}
                 {this.props.footer}
             </section>)
-        }
+
     }
 
 
